9ae121adb91d6c90dc229df7c513b2f79fd0734f
Jesse Monroy <jessemonroy650@yahoo.com>
2016-02-15 00:59:16 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//
+//
+//
+var app = {
+    onDeviceReady : function() {
+        if (device.platform === "iOS") {
+            // hide Exit button. They don't have one on iOS devices.
+            document.getElementById('exitApp').classList.add("hidden");
+            /* deals with post-iOS-7 change that covers the status bar */
+            document.body.style.marginTop = "20px";
+        } else if (device.platform == 'Android') {
+            document.getElementById('uuid').style.fontSize = 'medium';
+            document.getElementById('exitApp').addEventListener('click', function() {
+                navigator.app.exitApp();
+            });
+        }
+        //
+        phonegapStuff();
+    }
+}
+
+
+
+/*
+
+    window.location.href returns the href (URL) of the current page
+    window.location.hostname returns the domain name of the web host
+    window.location.pathname returns the path and filename of the current page
+    window.location.protocol returns the web protocol used (http:// or https://)
+    window.location.assign loads a new document
+*/
+
+function screenStuff() {
+    var screenStuff = "" +
+    "<p class=r />screen.width: "       + screen.width +
+	"<p class=g />screen.height: "      + screen.height +
+	"<p class=b />screen.availWidth: "  + screen.availWidth +
+	"<p class=r />screen.availHeight: " + screen.availHeight +  
+	"<p class=g />screen.colorDepth: "  + screen.colorDepth + 
+	"<p class=b />screen.pixelDepth: "  + screen.pixelDepth;  
+
+    document.getElementById('screenStuff').innerHTML = screenStuff;
+}
+
+function navigatorStuff() {
+    var navigatorStuff = "" +
+	"<p class=a />navigator.appCodeName: "   + navigator.appCodeName +
+	"<p class=p />navigator.appName: "       + navigator.appName +
+	"<p class=a />navigator.appVersion: "    + navigator.appVersion +
+	"<p class=p />navigator.cookieEnabled: " + navigator.cookieEnabled +  
+	"<p class=a />navigator.language: "      + navigator.language +
+	"<p class=p />navigator.userAgent: "     + navigator.userAgent +
+	"<p class=a />navigator.platform: "      + navigator.platform + 
+	"<p class=p />navigator.onLine: "        + navigator.onLine;  
+
+    document.getElementById('navigatorStuff').innerHTML = navigatorStuff;
+}
+
+
+function jqueryStuff() {
+    var jqueryStuff = "" +
+	"<p class=r />JQuery version is: "     +  $.fn.jquery +
+    "&nbsp;<p />" +
+	"<p class=p />$(window).innerWidth: "  + $(window).innerWidth() +
+	"<p class=a />$(window).innerHeight: " + $(window).innerHeight() +
+	"<p class=p />$(window).outerWidth: "  + $(window).outerWidth() +
+	"<p class=a />$(window).outerHeight: " + $(window).outerHeight();
+
+    document.getElementById('jqueryStuff').innerHTML = jqueryStuff;
+}
+
+function phonegapStuff() {
+    document.getElementById('cordova').innerHTML = device.cordova;
+    document.getElementById('model').innerHTML = device.model;
+    document.getElementById('platform').innerHTML = device.platform;
+    document.getElementById('uuid').innerHTML = device.uuid;
+    document.getElementById('version').innerHTML = device.version;
+}
+
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", app.onDeviceReady, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+    screenStuff();
+    navigatorStuff();
+    jqueryStuff();
+});
